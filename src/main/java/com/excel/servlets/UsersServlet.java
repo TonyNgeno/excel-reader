4eb24dccd063cfbb2341219218cfeb00163d869a
TonyNgeno <tonyngeno123@gmail.com>
2020-08-21 18:46:13 +0300

    minimal changes

--- src/main/java/com/excel/servlets/UsersServlet.java
+++ src/main/java/com/excel/servlets/UsersServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -31,6 +32,14 @@ public class UsersServlet extends HttpServlet {
     protected  void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         ServletContext scx = getServletContext();
         Connection dbConnection = (Connection) scx.getAttribute("dbConnection");
+        InputStream inputStream = null;
+        Part filePart = request.getPart("name");
+        if (filePart != null) {
+
+            // obtains input stream of the upload file
+            inputStream = filePart.getInputStream();
+        }
+
 
         String name = request.getParameter("name");
         int age = Integer.parseInt(request.getParameter("age"));
@@ -39,8 +48,17 @@ public class UsersServlet extends HttpServlet {
 
 
 
+
         try {
             PreparedStatement statement = dbConnection.prepareStatement("insert into users(name, age, town) values(?, ?, ?)");
+            /*if (inputStream != null) {
+                // fetches input stream of the upload file for the blob column
+                try {
+                    statement.setBlob(3, inputStream);
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            }*/
             statement.setString(1, name);
             statement.setInt(2, age);
             statement.setString(3, town);
