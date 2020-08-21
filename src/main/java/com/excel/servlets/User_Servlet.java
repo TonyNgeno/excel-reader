95be273a76ef14119c273551b0404e1e140e8e60
TonyNgeno <tonyngeno123@gmail.com>
2020-08-21 19:52:37 +0300

    minimal changes

--- src/main/java/com/excel/servlets/User_Servlet.java
+++ src/main/java/com/excel/servlets/User_Servlet.java
@@ -45,6 +45,7 @@ public class User_Servlet extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet UploadServlet at " + request.getContextPath() + "</h1>");
+            out.println( "<input type=\"file\" id=\"myfile\" name\"myfile\">");
             out.println("</body>");
             out.println("</html>");
         } finally {
